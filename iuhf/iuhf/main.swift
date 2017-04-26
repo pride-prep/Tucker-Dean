1f02acd46d1618c06d0ae59673776474915dcfc5
FreshBaconMan <tucker@my.prideprepschool.org>
2017-04-26 13:16:56 -0700

    Add files via upload

---
+++ iuhf/iuhf/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  iuhf
+//
+//  Created by Student on 4/26/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import Foundation
+
+print("Welcome to the book quiz.")
+print("What was the first book in the Harry Potter series?")
+print("A) Sorcerers Stone")
+print("B) Chamber of Secrets ")
+var answer = readLine()
+if (answer == "A") {
+    print("Congratulations, you are right!")
+}
+else if (answer == "B") {
+    print("You are wrong!")
+}
+
